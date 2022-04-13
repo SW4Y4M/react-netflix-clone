92cacb7cdf9b6683f24503071c796cc7879fa967
SW4Y4M <swayam.shashwat@gmail.com>
2022-04-14 00:03:04 +0530

    Updates

---
+++ precache-manifest.1086d47d60805bf405fd692ca2d2d2a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477af312e4749ddb99228412529f17ad",
+    "url": "/react-netflix-clone/index.html"
+  },
+  {
+    "revision": "59016e431b7a6d9e9a25",
+    "url": "/react-netflix-clone/static/css/main.b548183b.chunk.css"
+  },
+  {
+    "revision": "7e8cff166696c1e6c6ed",
+    "url": "/react-netflix-clone/static/js/2.248365c2.chunk.js"
+  },
+  {
+    "revision": "943aec86c35bb3f39d18124af76a452f",
+    "url": "/react-netflix-clone/static/js/2.248365c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59016e431b7a6d9e9a25",
+    "url": "/react-netflix-clone/static/js/main.bfa4e40a.chunk.js"
+  },
+  {
+    "revision": "33f93c9afead4b25d356",
+    "url": "/react-netflix-clone/static/js/runtime-main.bae1d50d.js"
+  }
+]);
